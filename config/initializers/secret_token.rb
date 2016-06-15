d6b4e29b43f3cd84eadd3ccc95a161358c6fd67f
Vineeth Pradhan <vineeth.pradhan@accenture.com>
2016-06-15 22:04:59 +0530

    Initialize

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Loginapp::Application.config.secret_token = 'd78520fda8ba07194f182aca33ef8106229f97f6c7c270ba890231f5fe570aae899c4d4e0758f982dec64b81cc3e9e5bf5acd04e41b8a99e68e04c1561550071'
